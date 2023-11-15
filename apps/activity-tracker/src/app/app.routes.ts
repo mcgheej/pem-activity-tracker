6ee615453aaf41679e55bd633558bc911b4c0dea
mcghee.j@outlook.com <mcghee.j@outlook.com>
2023-11-15 16:23:37 +0000

    Add login feature and space for status bar

--- apps/activity-tracker/src/app/app.routes.ts
+++ apps/activity-tracker/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Route } from '@angular/router';
+import { afNotAuthGuard } from '@pem-activity-tracker/af-authentication';
 import { HomeComponent } from '@pem-activity-tracker/home';
 import { PageNotFoundComponent } from '@pem-activity-tracker/shell';
 
 export const appRoutes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent, title: 'Home Page' },
+  {
+    path: 'login',
+    canActivate: [afNotAuthGuard('/home')],
+    loadChildren: () =>
+      import('@pem-activity-tracker/login').then((m) => m.loginRoutes),
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
---
+++ libs/activity-tracker/feature/login/src/lib/login/login.component.ts
@@ -0,0 +1,94 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroupDirective,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AfAuthenticationService } from '@pem-activity-tracker/af-authentication';
+
+interface UserDetails {
+  email: string | null;
+  password: string | null;
+}
+
+@Component({
+  selector: 'pem-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.component.html',
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {
+  private fb: FormBuilder = inject(FormBuilder);
+  private router: Router = inject(Router);
+  private snackBar: MatSnackBar = inject(MatSnackBar);
+  private afAuth: AfAuthenticationService = inject(AfAuthenticationService);
+
+  loginForm = this.fb.group({
+    email: [
+      '',
+      {
+        validators: [Validators.required, Validators.email],
+      },
+    ],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  getEmailError(): string {
+    if (this.loginForm.controls.email.hasError('required')) {
+      return 'You must enter an email address';
+    }
+    return this.loginForm.controls.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
+
+  getPasswordError(): string {
+    if (this.loginForm.controls.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return this.loginForm.controls.password.hasError('minlength')
+      ? 'Password must be at least 8 characters'
+      : '';
+  }
+
+  onSubmit(value: Partial<UserDetails>, formDirective: FormGroupDirective) {
+    const email = value.email ?? '';
+    const password = value.password ?? '';
+
+    this.afAuth.login(email, password).subscribe({
+      next: () => this.router.navigateByUrl('/activities'),
+      error: () => {
+        formDirective.resetForm();
+        this.loginForm.reset();
+        this.snackBar.open('Invalid username/password', undefined, {
+          duration: 2000,
+        });
+      },
+    });
+  }
+
+  onForgotPassword() {
+    this.snackBar.open('Forgot password not implemented', undefined, {
+      duration: 2000,
+    });
+  }
+}
