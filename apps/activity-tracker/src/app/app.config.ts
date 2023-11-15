a20a5a9a8935e10e8f1a08b4d6ed9fcb1881b544
mcghee.j@outlook.com <mcghee.j@outlook.com>
2023-11-15 10:46:20 +0000

    Added angular/fire and firebase

--- apps/activity-tracker/src/app/app.config.ts
+++ apps/activity-tracker/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { firebaseConfig } from '@pem-activity-tracker/firebase-config';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimations()],
+  providers: [
+    provideRouter(appRoutes),
+    provideAnimations(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
---
+++ libs/activity-tracker/util/firebase-config/src/lib/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCX59oaPiYouwTPvGAC7gDxK4DGQXZuZ2k',
+  authDomain: 'activity-tracker-3f7bd.firebaseapp.com',
+  projectId: 'activity-tracker-3f7bd',
+  storageBucket: 'activity-tracker-3f7bd.appspot.com',
+  messagingSenderId: '745583999275',
+  appId: '1:745583999275:web:3c0991801a287a9ae8ad6e',
+  measurementId: 'G-FW5FN3FYDV',
+};
